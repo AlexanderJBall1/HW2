3a69cbe762bbba47bd18626ba2691237d67f9e82
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-09-18 12:24:28 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -9,11 +9,11 @@ if(isset($_POST['my-name'])){
 ?>
   <p>The value sent is:</p>
 <?php
-  echo $_POST['my-name']
+  echo $_POST['my-name'];
 } else{
 ?>
   <p>Nothing posted to the page.</p>
 <?php
 }
-include "view-footer.php"
+include "view-footer.php";
 ?>
